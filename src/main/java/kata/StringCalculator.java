bd114ce1827c1d48b2472a135ee44269b4d13a07
dhruval.patel <dhruval.patel@eclinicalworks.com>
2019-07-26 07:40:42 -0400

    Implementation and refactor is done for empty string and  one number

--- src/main/java/kata/StringCalculator.java
+++ src/main/java/kata/StringCalculator.java
@@ -2,11 +2,16 @@ package kata;
 
 public class StringCalculator {
 	public static int add(String numbers) {
+		int total = 0;
 		if(null == numbers || numbers.isEmpty())
 		{
-			return 0;
+			return total;
 		}
-		return 0;
+		if(numbers.length() == 1)
+		{
+			 total = Integer.parseInt(numbers);
+		}
+		return total;
 		
 	}
 }
